0611965b04ecddecd8eae5d5a2e8096a92785729
tentato <alepaw99@gmail.com>
2023-05-21 14:39:02 +0200

    lab 2 finished

--- Lab2/zadania.py
+++ Lab2/zadania.py
@@ -1,48 +1,82 @@
+import re
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 from nltk.stem import WordNetLemmatizer
-from nltk.stem.arlstem import ARLSTem
 from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.base import clone
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import PrecisionRecallDisplay
+from sklearn.metrics import precision_recall_fscore_support
 
 #### ZADANIE 1 #######################################################################
 
-# tokenizer = nltk.tokenize.TreebankWordTokenizer()
-# s_words = stopwords.words("english")
-# stemmer = SnowballStemmer("english")
-# # stemmer = ARLSTem()
-# lemmatizer = WordNetLemmatizer()
+tokenizer = nltk.tokenize.TreebankWordTokenizer()
+s_words = stopwords.words("english")
+stemmer = SnowballStemmer("english")
+lemmatizer = WordNetLemmatizer()
 
-# text = pd.read_csv("imbd.csv",delimiter=",")
+text = pd.read_csv("imbd.csv",delimiter=",")
 
-# text.content = text.content.apply(str.lower)
-# text["tokens"] = text.content.apply(tokenizer.tokenize)
-# tokens_no_stopwords = []
-# for sentence in text["tokens"]:
-#     tokens_no_stopwords.append([word for word in sentence if word not in s_words])
-# text["tokens_no_stopwords"] = tokens_no_stopwords
+text.content = text.content.apply(str.lower)
+raw_tokens = text.content.apply(tokenizer.tokenize)
+clear_tokens = []
+for tokens in raw_tokens:
+    clear_token = []
+    for token in tokens:
+        r = re.compile(r"[^a-zA-Z0-9]+")
+        token = r.sub("", token)
+        clear_token.append(token)
+    clear_tokens.append(clear_token)
+text["tokens"] = clear_tokens
+    
+tokens_no_stopwords = []
+for sentence in text["tokens"]:
+    tokens_no_stopwords.append([word for word in sentence if word not in s_words])
+text["tokens_no_stopwords"] = tokens_no_stopwords
 
-# tokens_stemm = []
-# for sentence in text["tokens_no_stopwords"]:
-#     tokens_stemm.append([stemmer.stem(word) for word in sentence])
-# text["tokens_stemm"] = tokens_stemm
-# tokens_lem = []
+tokens_stemm = []
+for sentence in text["tokens_no_stopwords"]:
+    tokens_stemm.append([stemmer.stem(word) for word in sentence])
+text["tokens_stemm"] = tokens_stemm
+tokens_lem = []
 
-# for sentence in text["tokens_no_stopwords"]:
-#     tokens_lem.append([stemmer.stem(word) for word in sentence])
-# text["tokens_lem"] = tokens_lem
+for sentence in text["tokens_no_stopwords"]:
+    tokens_lem.append([stemmer.stem(word) for word in sentence])
+text["tokens_lem"] = tokens_lem
 
-# text.to_csv("result.csv", index=False)
-# print(f"Results saved as result.csv")
+text["tokens"] = [" ".join(tokens) for tokens in text["tokens"]]
+text["tokens_no_stopwords"] = [" ".join(tokens) for tokens in text["tokens_no_stopwords"]]
+text["tokens_stemm"] = [" ".join(tokens) for tokens in text["tokens_stemm"]]
+text["tokens_lem"] = [" ".join(tokens) for tokens in text["tokens_lem"]]
+
+clear_tokens = []
+clear_tokens_sw = []
+clear_tokens_st = []
+clear_tokens_le = []
+for sentence in text["tokens"]:
+    sentence = re.sub(' +', ' ', sentence)
+    sentence = clear_tokens.append(sentence)
+for sentence in text["tokens_no_stopwords"]:
+    sentence = re.sub(' +', ' ', sentence)
+    sentence = clear_tokens_sw.append(sentence)
+for sentence in text["tokens_stemm"]:
+    sentence = re.sub(' +', ' ', sentence)
+    sentence = clear_tokens_st.append(sentence)
+for sentence in text["tokens_lem"]:
+    sentence = re.sub(' +', ' ', sentence)
+    sentence = clear_tokens_le.append(sentence)
+
+text["tokens"] = clear_tokens
+text["tokens_no_stopwords"] =clear_tokens_sw
+text["tokens_stemm"] = clear_tokens_st
+text["tokens_lem"] = clear_tokens_le
+
+text.to_csv("result.csv", index=False)
+print(f"Results saved as result.csv")
 
 #### ZADANIE 2 #######################################################################
 
@@ -62,7 +96,7 @@ for set in sets:
 
         y_pred = model.predict(cv.transform(X_test).toarray())
         score = accuracy_score(y_test, y_pred)
-        print(f"Model: {model}, \tDataset: {set}, \tAccuracy score: {score}")
+        print(f"Model: {model}, Dataset: {set}, Accuracy score: {score}\n")
 
 # Obserwacje:        
 #   Wyniki dla klasyfikatora SVC są lepsze niż dla MLP.
@@ -70,26 +104,34 @@ for set in sets:
 
 ##### ZADANIE 3
 
+dataset = pd.read_csv("result.csv",delimiter=",")
+random_state = 10
+sets = ["tokens","tokens_no_stopwords","tokens_stemm","tokens_lem"]
+model = MLPClassifier()
+
+set_results = []
+
 for set in sets:
-    for model in models:
-        print(f"Model: {model}")
-        
-        tfidf = TfidfVectorizer()
-        X = tfidf.fit_transform(dataset[set]).toarray()
-        y = dataset.label
-        
-        kfold = RepeatedStratifiedKFold(n_splits=2, n_repeats=5,random_state=random_state)
-        splits = kfold.split(X,y)
+    
+    tfidf = TfidfVectorizer()
+    X = tfidf.fit_transform(dataset[set]).toarray()
+    y = dataset.label
+    
+    kfold = RepeatedStratifiedKFold(n_splits=2, n_repeats=5, random_state=random_state)
+    splits = kfold.split(X,y)
+    
+    folds_results = []
+    for n,(train_index,test_index) in enumerate(splits):
+        x_train_fold, x_test_fold = X[train_index], X[test_index]
+        y_train_fold, y_test_fold = y[train_index], y[test_index]
+        model.fit(x_train_fold, y_train_fold)
 
-        for n,(train_index,test_index) in enumerate(splits):
-            x_train_fold, x_test_fold = X[train_index], X[test_index]
-            y_train_fold, y_test_fold = y[train_index], y[test_index]
-            model.fit(x_train_fold, y_train_fold)
+        y_pred = model.predict(x_test_fold)
+        # score = accuracy_score(y_test_fold, y_pred)
+        score = precision_recall_fscore_support(y_test_fold, y_pred, average='weighted')
+        # print(f"Score: {score}, n: {n}")
+        folds_results.append(score)
 
-            y_pred = model.predict(x_test_fold)
-            score = accuracy_score(y_test_fold, y_pred)
-            print(f"Score: {score}")
-            # display = PrecisionRecallDisplay.from_estimator(
-            #     classifier, X_test, y_test, name="LinearSVC"
-            # )
-            # _ = display.ax_.set_title("2-class Precision-Recall curve")
+    set_results.append(folds_results)
+
+print(set_results)
